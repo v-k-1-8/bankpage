07b292435bef69ebdc5016e025f1c8bf40ca08fb
v-k-1-8 <lakhiyanivarun14@gmail.com>
2025-03-02 23:52:36 +0530

    React Site completed

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";  
+import Stats from "./Stats";
+import Business from "./Business";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Testimonials from "./Testimonials";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Button from "./Button";
+import GetStarted from "./GetStarted";
+import FeedbackCard from "./FeedbackCard";
+
+export  {
+    Navbar,
+    Hero,
+    Stats,
+    Business,
+    Billing,
+    CardDeal,
+    Testimonials,
+    Clients,
+    CTA,
+    Footer,
+    Button,
+    GetStarted,
+    FeedbackCard,
+};
